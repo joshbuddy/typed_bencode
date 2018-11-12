7d425c80f160957083cf77e53c1a8f8c1f65d262
joshbuddy <joshbuddy@gmail.com>
2018-11-12 12:18:42 -0600

    Reformat

--- test/test_encoding.py
+++ test/test_encoding.py
@@ -3,30 +3,34 @@ import typed_bencode
 from typing import List
 from unittest import TestCase
 
+
 class TestEncoding(TestCase):
     def test_simple_encoding(self):
         my_type = typed_bencode.for_dict(a=str, b=int, c=bytes, d=List[str])
-        val = {"a": "hello", "b": 123, "c": b'asd', "d":["hey", "there"]}
+        val = {"a": "hello", "b": 123, "c": b"asd", "d": ["hey", "there"]}
         encoded = my_type.encode(val)
-        self.assertEqual(encoded, b'd1:a5:hello1:bi123e1:c3:asd1:dl3:hey5:thereee')
+        self.assertEqual(encoded, b"d1:a5:hello1:bi123e1:c3:asd1:dl3:hey5:thereee")
 
     def test_complex_encoding(self):
         my_sub_type = typed_bencode.for_dict(a=str, b=int)
         my_type = typed_bencode.for_dict(a=str, b=int, c=bytes, d=my_sub_type)
-        val = {"a": "hello", "b": 123, "c": b'asd', "d":{"a": "asd", "b":321}}
+        val = {"a": "hello", "b": 123, "c": b"asd", "d": {"a": "asd", "b": 321}}
         encoded = my_type.encode(val)
-        self.assertEqual(encoded, b'd1:a5:hello1:bi123e1:c3:asd1:dd1:a3:asd1:bi321eee')
+        self.assertEqual(encoded, b"d1:a5:hello1:bi123e1:c3:asd1:dd1:a3:asd1:bi321eee")
 
     def test_default_encoding(self):
         my_type = typed_bencode.for_dict(a=str, b=int, c=bytes).default(b=321)
-        val = {"a": "hello", "c": b'asd', "d":["hey", "there"]}
+        val = {"a": "hello", "c": b"asd", "d": ["hey", "there"]}
         encoded = my_type.encode(val)
-        self.assertEqual(encoded, b'd1:a5:hello1:bi321e1:c3:asde')
+        self.assertEqual(encoded, b"d1:a5:hello1:bi321e1:c3:asde")
 
     def test_missing_value(self):
         my_type = typed_bencode.for_dict(a=str, b=int, c=bytes)
         val = {"a": "hello"}
-        with self.assertRaises(ValueError, msg="attempt to encode a dict, missing 'b', and not present in defaults"):
+        with self.assertRaises(
+            ValueError,
+            msg="attempt to encode a dict, missing 'b', and not present in defaults",
+        ):
             my_type.encode(val)
 
     def test_custom_type(self):
@@ -48,7 +52,6 @@ class TestEncoding(TestCase):
         my_type = DateType()
         val = datetime.datetime.now()
         encoded = my_type.encode(val)
-        print(encoded)
         self.assertEqual(encoded, f"26:{val.isoformat()}".encode())
         decoded = my_type.decode(encoded)
         self.assertEqual(decoded, val)
--- typed_bencode/__init__.py
+++ typed_bencode/__init__.py
@@ -2,20 +2,26 @@ from typing import List
 from io import BytesIO
 from abc import ABC
 
+
 def for_dict(**kwargs):
     return DictType(**kwargs)
+
 
 def for_list(**kwargs):
     return ListType(**kwargs)
 
+
 def for_str(**kwargs):
     return StringType(**kwargs)
+
 
 def for_int(**kwargs):
     return IntType(**kwargs)
 
+
 def for_bytes(**kwargs):
     return BytesType(**kwargs)
+
 
 def wrap_type(t):
     if isinstance(t, BaseType):
@@ -31,6 +37,7 @@ def wrap_type(t):
     else:
         raise Exception(f"could not construct a type for {t}")
 
+
 class BaseType(ABC):
     def __init__(self):
         self.defaults = {}
@@ -42,13 +49,16 @@ class BaseType(ABC):
         out, _ = self.decoder.from_bytes(b)
         return out
 
+
 class BaseEncoder(ABC):
     def __init__(self, type):
         self.type = type
 
+
 class BaseDecoder(ABC):
     def __init__(self, type):
         self.type = type
+
 
 class IntType(BaseType):
     def __init__(self):
@@ -56,11 +66,13 @@ class IntType(BaseType):
         self.encoder = IntEncoder(self)
         self.decoder = IntDecoder(self)
 
+
 class StringType(BaseType):
     def __init__(self):
         super().__init__()
         self.encoder = StringEncoder(self)
         self.decoder = StringDecoder(self)
+
 
 class BytesType(BaseType):
     def __init__(self):
@@ -68,12 +80,14 @@ class BytesType(BaseType):
         self.encoder = BytesEncoder(self)
         self.decoder = BytesDecoder(self)
 
+
 class ListType(BaseType):
     def __init__(self, subtype):
         super().__init__()
         self.subtype = wrap_type(subtype)
         self.encoder = ListEncoder(self)
         self.decoder = ListDecoder(self)
+
 
 class DictType(BaseType):
     def __init__(self, **kwargs):
@@ -92,10 +106,11 @@ class DictType(BaseType):
             self.defaults[name] = default
         return self
 
+
 class DictEncoder(BaseEncoder):
     def to_bytes(self, values):
         out = BytesIO()
-        out.write(b'd')
+        out.write(b"d")
         for name in self.type.field_names:
             wrapped_type = self.type.fields[name]
             if name in values:
@@ -103,13 +118,16 @@ class DictEncoder(BaseEncoder):
             elif name in self.type.defaults:
                 value = self.type.defaults[name]
             else:
-                raise ValueError(f"attempt to encode a dict, missing '{name}', and not present in defaults")
+                raise ValueError(
+                    f"attempt to encode a dict, missing '{name}', and not present in defaults"
+                )
             out.write(str(len(name)).encode())
-            out.write(b':')
+            out.write(b":")
             out.write(name.encode())
             out.write(wrapped_type.encoder.to_bytes(value))
-        out.write(b'e')
+        out.write(b"e")
         return out.getvalue()
+
 
 class DictDecoder(BaseDecoder):
     def from_bytes(self, b, pos=0):
@@ -117,60 +135,68 @@ class DictDecoder(BaseDecoder):
         pos += 1
         out = {}
         while pos < len(b) - 1:
-            if b[pos] == 101: # e
+            if b[pos] == 101:  # e
                 break
             key, pos = StringDecoder(None).from_bytes(b, pos)
             wrapped_type = self.type.fields[key]
             val, pos = wrapped_type.decoder.from_bytes(b, pos)
             out[key] = val
         assert b[pos] == 101, f"expected byte at {pos} {b[pos]} to be 101 (e)"
-        return (out, pos+1)
+        return (out, pos + 1)
+
 
 class StringEncoder(BaseEncoder):
     def to_bytes(self, value):
         return f"{len(value)}:{value}".encode()
 
+
 class StringDecoder(BaseDecoder):
     def from_bytes(self, b, pos=0):
-        sep_index = b.find(b':', pos)
+        sep_index = b.find(b":", pos)
         length = int(b[pos:sep_index].decode())
         start_index = sep_index + 1
         end_index = start_index + length
         return (b[start_index:end_index].decode(), end_index)
 
+
 class BytesEncoder(BaseEncoder):
     def to_bytes(self, value):
         return f"{len(value)}:".encode() + value
 
+
 class BytesDecoder(BaseDecoder):
     def from_bytes(self, b, pos=0):
-        sep_index = b.find(b':', pos)
+        sep_index = b.find(b":", pos)
         length = int(b[pos:sep_index].decode())
         start_index = sep_index + 1
         end_index = start_index + length
         return (b[start_index:end_index], end_index)
 
+
 class IntEncoder(BaseEncoder):
     def to_bytes(self, value):
         return f"i{value}e".encode()
 
+
 class IntDecoder(BaseEncoder):
     def from_bytes(self, b, pos=0):
         assert b[pos] == 105, f"expected byte at {pos} {b[pos]} to be 105 (i)"
-        end_index = b.find(b'e', pos)
-        value = int(b[pos+1:end_index].decode())
+        end_index = b.find(b"e", pos)
+        value = int(b[pos + 1 : end_index].decode())
         pos = end_index
         assert b[pos] == 101, f"expected byte at {pos} {b[pos]} to be 101 (e)"
-        return (value, pos+1)
+        return (value, pos + 1)
+
 
 class ListEncoder(BaseEncoder):
     def to_bytes(self, values):
         out = BytesIO()
-        out.write(b'l')
+        out.write(b"l")
         for v in values:
             out.write(self.type.subtype.encoder.to_bytes(v))
-        out.write(b'e')
+        out.write(b"e")
         return out.getvalue()
+
 
 class ListDecoder(BaseDecoder):
     def from_bytes(self, b, pos=0):
@@ -178,9 +204,9 @@ class ListDecoder(BaseDecoder):
         pos += 1
         out = []
         while pos < len(b) - 1:
-            if b[pos] == 101: # e
+            if b[pos] == 101:  # e
                 break
             val, pos = self.type.subtype.decoder.from_bytes(b, pos)
             out.append(val)
         assert b[pos] == 101, f"expected byte at {pos} {b[pos]} to be 101 (e)"
-        return (out, pos+1)
+        return (out, pos + 1)
